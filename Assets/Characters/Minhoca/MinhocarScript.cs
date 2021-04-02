2420e44f159b9b722192d8a2f15fe01230a52e8b
Mateus GitHub <mateusasfreitas@gmail.com>
2021-04-01 21:46:06 -0300

    Aula 6 - 01.04.2021 - Adicionado colisão nos character, text e Minhoca

---
+++ Assets/Characters/Minhoca/MinhocarScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinhocarScript : MonoBehaviour
+{
+    public float velocity;
+    private Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.body = GetComponent<Rigidbody2D>();
+        this.body.velocity = new Vector2(this.velocity, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
