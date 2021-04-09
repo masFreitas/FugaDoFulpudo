4aa451c27fa9d5b96da7d18384aed15ad63a87bc
Mateus GitHub <mateusasfreitas@gmail.com>
2021-04-08 21:37:28 -0300

    Aula 08 - 08.04.2021 - Adicionato pontuação, colisão no teto e chão e spawn de inimgo

--- Assets/Characters/Minhoca/MinhocarScript.cs
+++ Assets/Characters/Minhoca/MinhocarScript.cs
@@ -6,16 +6,33 @@ public class MinhocarScript : MonoBehaviour
 {
     public float velocity;
     private Rigidbody2D body;
-    // Start is called before the first frame update
+    private GameObject player, gameController;
+    private bool  scoreUp;
     void Start()
     {
         this.body = GetComponent<Rigidbody2D>();
         this.body.velocity = new Vector2(this.velocity, 0);
+        this.player = GameObject.FindGameObjectWithTag("Player");
+        this.gameController = GameObject.FindGameObjectWithTag("GameController");
+        this.scoreUp = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (this.transform.position.x < -15)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            if (this.transform.position.x < this.player.transform.position.x)
+            {
+                if (!this.scoreUp)
+                {
+                    this.gameController.SendMessage("ScorePlusOne");
+                    this.scoreUp = true;
+                }
+            }
+        }
     }
 }
--- Assets/GameControllerScript.cs
+++ Assets/GameControllerScript.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameControllerScript : MonoBehaviour
 {
     public Text mainText;
+    public Text scoreText;
+    public GameObject foe;
+    private int score;
     void Start()
     {
-        
+        this.scoreText.text = string.Empty;
     }
 
     // Update is called once per frame
@@ -19,11 +23,34 @@ public class GameControllerScript : MonoBehaviour
 
     void StartGame()
     {
+        this.score = 0;
+        this.scoreText.text = this.score.ToString();
         this.mainText.text = string.Empty;
+        InvokeRepeating("SummonFoe", 1, 1.5f);
+        
     }
 
     void EndGame()
     {
         this.mainText.text = "Game Over!";
+        CancelInvoke("SummonFoe");
+    }
+
+    void ScorePlusOne()
+    {
+        this.score += 1;
+        this.scoreText.text = this.score.ToString();
+    }
+
+    void SummonFoe()
+    {
+        float randomWidth = 10 * Random.value - 5;
+        GameObject newFoe = Instantiate(foe);
+        newFoe.transform.position = new Vector2(15, randomWidth);
+    }
+
+    void RestartGame()
+    {
+        SceneManager.LoadScene("InfinityPhase");
     }
 }
